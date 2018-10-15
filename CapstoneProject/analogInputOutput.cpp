0895c1f4b6e957473d932d534a134909e7369dfd
souljt01 <souljt01@gmail.com>
2018-10-14 21:48:58 -0700

    apply the Soil Moisture level

--- CapstoneProject/analogInputOutput.cpp
+++ CapstoneProject/analogInputOutput.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
 #include "analogInputOutput.h"
 #include "waterPump.h"
+#include "blinkLed.h"
 
 // Data of Soil Humidity Sensor
+//input voltages between 0 and 5 volts into integer values between 0 and 1023
+#define MAX_INTEGER_VOLT_VALUE 1023
+
+//Max raw value for 100% moisture is 800.
+#define MAX_MOISTURE_VALUE 800
+
+//Turn on Water Pump 
+#define TRUR_ON_PUMP_THRESHOLD_VALUE 25
+
+int soilMoistureRawVal = 0;
+int soilMoisturePercentage = 0;
 int analogReadHumidityPin = 0;     // connected to analog pin 0
                                    // outside leads to ground and +5V
 int analogWriteHumidityPin = 8; 
@@ -30,17 +42,33 @@ void analogReadHumidity(void)
 {
   analogReadHumidityValue = analogRead(analogReadHumidityPin);     // read the input pi
 
-  Serial.println("Humidity Value:");
-  Serial.println(analogReadHumidityValue);             // debug value
-    if (analogReadHumidityValue > 512)
-    {
-      Serial.println(" => Turn on the water pump");
+  soilMoistureRawVal = MAX_INTEGER_VOLT_VALUE - analogReadHumidityValue;
+
+  if (soilMoistureRawVal < MAX_MOISTURE_VALUE){
+    soilMoisturePercentage = (soilMoistureRawVal/MAX_MOISTURE_VALUE)*100;
+  }
+
+  if (soilMoisturePercentage <= TRUR_ON_PUMP_THRESHOLD_VALUE){
+      Serial.println(" => Turn on Water Pump");
+      ledOn();
       waterPumpOn();
-    }
-    else {
-      Serial.println(" => Turn off the water pump");
+  }
+   else {
+      Serial.println(" => Turn off the Water Pump");
+      ledOff();
       waterPumpOff();
-    }
+  }
+//  Serial.println("Humidity Value:");
+//  Serial.println(analogReadHumidityValue);             // debug value
+//    if (analogReadHumidityValue > 512)
+//    {
+//      Serial.println(" => Turn on the water pump");
+//      waterPumpOn();
+//    }
+//    else {
+//      Serial.println(" => Turn off the water pump");
+//      waterPumpOff();
+//    }
   delay(500);
 }
 
